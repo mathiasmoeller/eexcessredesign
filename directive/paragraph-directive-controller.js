8c7a60023d666edfe6e1f9e3698a92396baf1551
mathias <matze.moeller@gmail.com>
2015-04-26 12:16:34 +0200

    proceeded with paragraph directive design

--- directive/paragraph-directive-controller.js
+++ directive/paragraph-directive-controller.js
@@ -1,24 +1,50 @@
 (function () {
     'use strict';
 
-    function ParagraphCtrl($scope, $sce) {
-        $scope.iconSource = $sce.trustAsResourceUrl('chrome-extension://' + ER.utils.extID + '/media/icons/query-icon.svg');
+    function ParagraphCtrl($scope, $sce, $mdDialog) {
+        $scope.icons = {};
+        $scope.icons.query = $sce.trustAsResourceUrl('chrome-extension://' + ER.utils.extID + '/media/icons/query-icon.svg');
+        $scope.icons.image = $sce.trustAsResourceUrl('chrome-extension://' + ER.utils.extID + '/media/icons/image-icon.svg');
+        $scope.icons.text = $sce.trustAsResourceUrl('chrome-extension://' + ER.utils.extID + '/media/icons/text-icon.svg');
+        $scope.icons.video = $sce.trustAsResourceUrl('chrome-extension://' + ER.utils.extID + '/media/icons/video-icon.svg');
+
         $scope.showQueryButton = false;
         $scope.keywords = [];
+        var results = {};
 
         $scope.query = function () {
-            // outgoing paragraph has to be in a list. this is requested by the api of the REST service
-            var paragraph = [ER.paragraphs.getParagraph($scope.id)];
-            $scope.queried = true;
+            if ($scope.keywords.length === 0) {
 
-            ER.messaging.callBG({
-                method: {parent: 'keywords', func: 'getParagraphEntities'},
-                data: paragraph
-            }, function (result) {
-                $scope.keywords = result.paragraphs[0].statistic;
-                console.log(result);
-            });
+                // outgoing paragraph has to be in a list. this is requested by the api of the REST service
+                var paragraph = [ER.paragraphs.getParagraph($scope.id)];
+                $scope.queried = true;
+
+                ER.messaging.callBG({
+                    method: {parent: 'keywords', func: 'getParagraphEntities'},
+                    data: paragraph
+                }, function (result) {
+                    $scope.keywords = result.paragraphs[0].statistic;
+                    console.log(result);
+                });
+            }
+
+            // Query europeana
         };
+
+        $scope.showTextResults= function(event) {
+            $mdDialog.show({
+                templateUrl: $sce.trustAsResourceUrl('chrome-extension://' + ER.utils.extID + '/result-dialog/result-dialog.html'),
+                controller: 'ResultDialogCtrl',
+                resolve: {
+                    results: function () {
+                        return results;
+                    }
+                },
+                targetEvent: event
+            }).then(function (changedObject) {
+
+            });
+        }
     }
 
     angular
