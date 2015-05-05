de9c400df7123f0c1d468f0d3c8e52f7e959d772
mathias <matze.moeller@gmail.com>
2015-05-05 19:21:26 +0200

    popup now loads and save the application flag

--- popup/popup-controller.js
+++ popup/popup-controller.js
@@ -4,16 +4,28 @@
     function PopupCtrl($scope) {
         var extID = chrome.i18n.getMessage('@@extension_id');
         $scope.linkToSettings = "chrome://extensions?options=" + extID;
-        console.log($scope.linkToSettings);
+        $scope.application = {};
+        chrome.storage.sync.get('eRedesign', function (data) {
+            chrome.tabs.query({active: true}, function (tab) {
+                var tabID = tab[0].id;
+                if (data && data.eRedesign && data.eRedesign[tabID] !== undefined) {
+                    $scope.application.showApp = data.eRedesign[tabID];
+                    console.log("yey");
+                }
+                else {
+                    $scope.application.showApp = true;
+                    console.log("no");
+                }
+                $scope.$apply();
+            });
 
-        $scope.toggleApplication = true;
+        });
 
-        $scope.$watch('toggleApplication', function () {
-            console.log("watcher");
+        $scope.$watch('application.showApp', function () {
             chrome.tabs.query({active: true}, function (tab) {
                 var tabID = tab[0].id;
                 var eRedesign = {};
-                eRedesign[tabID] = $scope.toggleApplication;
+                eRedesign[tabID] = $scope.application.showApp;
 
                 chrome.storage.sync.set({'eRedesign': eRedesign}, function () {
                     console.log("success");
@@ -25,4 +37,5 @@
     angular
         .module('eRedesignPopup')
         .controller('PopupCtrl', PopupCtrl);
-})();
+})
+();
