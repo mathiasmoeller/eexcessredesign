317ced0b7c0e9199d67cfb47cb56afcceb569032
mathias <matze.moeller@gmail.com>
2015-05-05 19:40:14 +0200

    added comments to the popup controller and removed console logs

--- popup/popup-controller.js
+++ popup/popup-controller.js
@@ -4,23 +4,26 @@
     function PopupCtrl($scope) {
         var extID = chrome.i18n.getMessage('@@extension_id');
         $scope.linkToSettings = "chrome://extensions?options=" + extID;
+
+        // The application.showApp needs to be a unshallow object. Don't ask why. I dare you. I double dare you.
         $scope.application = {};
+
+        // Check if the plugin is disabled or enabled. If no data is found enable it (first startup)
         chrome.storage.sync.get('eRedesign', function (data) {
             chrome.tabs.query({active: true}, function (tab) {
                 var tabID = tab[0].id;
                 if (data && data.eRedesign && data.eRedesign[tabID] !== undefined) {
                     $scope.application.showApp = data.eRedesign[tabID];
-                    console.log("yey");
                 }
                 else {
                     $scope.application.showApp = true;
-                    console.log("no");
                 }
                 $scope.$apply();
             });
 
         });
 
+        // Save the value of showApp to the storage
         $scope.$watch('application.showApp', function () {
             chrome.tabs.query({active: true}, function (tab) {
                 var tabID = tab[0].id;
