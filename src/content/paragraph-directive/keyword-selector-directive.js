512845e8b86c7262615d1b4bba596af9808f5f7b
mathias <matze.moeller@gmail.com>
2015-06-05 15:54:22 +0200

    keyword-selector can now be toggled

--- src/content/paragraph-directive/keyword-selector-directive.js
+++ src/content/paragraph-directive/keyword-selector-directive.js
@@ -3,17 +3,45 @@
         return {
             restrict: 'A',
             scope: {
-              handleClick: '&'
+                handleClick: '&',
+                active: '='
             },
-            link: function(scope, element) {
-                element.css({ cursor: 'pointer' });
-                element.on('dblclick', function(e) {
-                    var range = window.getSelection() || document.getSelection() || document.selection.createRange();
-                    var word = range.toString().trim();
-                    if(word !== '') {
-                        scope.handleClick({keyword: word});
+            link: function (scope, element) {
+                scope.element = element;
+                if (scope.active) {
+                    element.css({cursor: 'pointer'});
+                    element.on('dblclick', function (e) {
+                        var range = window.getSelection() || document.getSelection() || document.selection.createRange();
+                        var word = range.toString().trim();
+                        if (word !== '') {
+                            scope.handleClick({keyword: word});
+                        }
+                    });
+                }
+            }, controller: function ($scope) {
+                $scope.$watch('active', function () {
+                    if ($scope.active) {
+                        _addProperties();
+                    } else {
+                        _removeProperties();
                     }
                 });
+
+                function _removeProperties() {
+                    $scope.element.css({cursor: 'default'});
+                    $scope.element.off('dblclick');
+                }
+
+                function _addProperties() {
+                    $scope.element.css({cursor: 'pointer'});
+                    $scope.element.on('dblclick', function (e) {
+                        var range = window.getSelection() || document.getSelection() || document.selection.createRange();
+                        var word = range.toString().trim();
+                        if (word !== '') {
+                            $scope.handleClick({keyword: word});
+                        }
+                    });
+                }
             }
         }
     }
